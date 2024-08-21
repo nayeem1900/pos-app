c9fef15da4f7619831a38e35a0bf7deac77c7be2
unknown <nayeembd84@gmail.com>
2024-08-21 11:24:56 +0600

    pos

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName',length:50);
+            $table->string('lasttName',length:50);
+            $table->string('email',length:50)->unique();
+            $table->string('mobile',length:50);
+            $table->string('otp',length:10);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password',length:50);
+            $table->rememberToken();
+            $table->timestamps('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
